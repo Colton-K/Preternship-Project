5e2901c26a2df1e0c756db54e192062415605852
Fangcong-Yin <fyin@nd.edu>
2020-10-27 18:19:44 -0400

    some updates

--- conversion.py
+++ conversion.py
@@ -41,6 +41,29 @@ def startOfCodeBlock(line):
   elif 'Fortran' in line:
     return "```fortran\n"
 
+#Add appropriate   `` around function names and parameters
+def adjustWords(words):
+    for index in range(len(words)):
+        last_mark = ''
+        #check function names
+        if '_' in words[index]:
+            #Move the punctuation out of ``
+            if(words[index][len(words[index])-1].isalnum()==False):
+                last_mark = words[index][len(words[index])-1]
+                words[index]=words[index][0:len(words[index])-1]
+            words[index]='`{}`'.format(words[index])
+        #check parameters
+        elif words[index][0]=='*' and words[index][len(words[index])-1] == '*':
+            if(words[index][len(words[index])-2].isalnum()==False):
+                last_mark = words[index][len(words[index])-2]
+                words[index]=words[index][0:len(words[index])-2]+words[index][len(words[index])-1:]
+            words[index]=words[index].replace('*','`')
+        #Delete unnecassary escape signs
+        elif '\\' in words[index]:
+            words[index]=words[index].replace('\\','')
+        words[index]+=last_mark
+    line = (' ').join(words)
+    return line
 # reads a markdown file and calls helper function processLine to process the markdown file further
 def adjustMarkdown(filename):
   workingLines = []
@@ -52,13 +75,19 @@ def adjustMarkdown(filename):
 
   inCodeBlock = False
   addText = False
+  #check whether it is in the name section
+  name = False
   for i in range(1, len(workingLines)):
     line = ""
 
+    #delete unnecassary blank lines
+    if workingLines[i].isspace():
+        continue
     # titles
-    if "====" in workingLines[i]:
+    elif "====" in workingLines[i]:
       if (inCodeBlock):
         newLines.append("```\n")
+        newLines.append('\n')
         inCodeBlock = False
 
       addText = False
@@ -66,12 +95,15 @@ def adjustMarkdown(filename):
       # if all caps, then heading 1
       if allUpper(workingLines[i-1]):
         if workingLines[i-1] != "NAME\n":
-          line = '\n# ' + workingLines[i-1].title()
+        #add a new line after each title
+          line = '\n# ' + workingLines[i-1].title()+'\n'
+          name = False
         else:
-          line = '# ' + workingLines[i-1].title()
+          line = '# ' + workingLines[i-1].title()+'\n'
+          name = True
       # else, heading 2
       else:
-        line = '## ' + workingLines[i-1].title()
+        line = '## ' + workingLines[i-1].title()+'\n'
 
     # indented blocks
     elif "    " in workingLines[i]:
@@ -81,8 +113,12 @@ def adjustMarkdown(filename):
         if "##" in newLines[len(newLines)-1]:
           newLines.append(startOfCodeBlock(newLines[len(newLines)-1]))
           line = workingLines[i][4:]
+
         else:
           line = workingLines[i][4:]
+          #When changing a new line in a code block, use six spaces instead of a tab
+          if(line[0]=='\t'):
+              line = '      '+line[1:]
       else:
         print("HERE")
         line = "-----------HERE----------------"
@@ -113,6 +149,9 @@ def adjustMarkdown(filename):
           addText = True
         elif 'See Also' in newLines[len(newLines)-1]:
           addText = True
+          #add notes section
+        elif 'Notes' in newLines[len(newLines)-1]:
+          addText = True
 
         # filter headers and blank lines
         if addText and not allUpper(workingLines[i]):
@@ -120,15 +159,26 @@ def adjustMarkdown(filename):
           if workingLines[i][len(workingLines[i]) - 2] == '\\':
             # Format: [`MPI_Bcast`(3)](MPI_Bcast.html)
             line = "[`{}`(3)]({}.html)\n".format(workingLines[i][:-2],workingLines[i][:-2])
-
           # normal text
           else:
-            line = workingLines[i]
+            #Filter function name and parameters in it
+            line =  workingLines[i]
+            #if a normal text is under name section, also add it to newLines
+      elif(name==True and workingLines[i].isupper()==False and workingLines[i]!='\n'):
 
-    # print(line)
+          line = workingLines[i]
+
+
+    else:
+        line =  workingLines[i]
+    #adjust words for each line
+    try:
+        line = adjustWords(line.split(' '))
+    except:
+        #if the line only has one word, skip this line
+        pass
     if(line):
       newLines.append(line)
-
   return newLines
 
 def runPandoc(file):
